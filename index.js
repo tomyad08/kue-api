15fe7399cf888b09c91d4e68a5a040a21eb2883a
LAPTOP-JF5K0512\tomyt <tomyad08@gmail.com>
2023-06-27 10:47:12 +0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 require("./src/routers/produk.routers")(app);
 
 const PORT = 8100;
