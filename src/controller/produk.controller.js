4e99c1b2e91b493a39459a2477c02c5fdd8ac088
tomyad08 <116580160+tomyad08@users.noreply.github.com>
2023-06-29 13:38:06 +0700

    Update produk.controller.js

--- src/controller/produk.controller.js
+++ src/controller/produk.controller.js
@@ -2,3 +2,14 @@ const produk = require("../data/data");
 exports.data = (req, res) => {
   res.json(produk.dataProduk);
 };
+exports.findOne = async (req, res) => {
+  const Id = Number(req.params.id);
+  const data = produk.dataProduk;
+
+  const FilterData = await data.filter((value) => {
+    if (value.id === Id) {
+      return value;
+    }
+  });
+  res.json(FilterData);
+};
